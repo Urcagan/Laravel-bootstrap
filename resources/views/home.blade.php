0b486b011697bba9433181821f4e320d2425f280
Dmitry Vystropov <beepper@mail.ru>
2022-09-01 10:36:46 +0300

    Add home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+<h1>Tutorial made by Positronx.io</h1>
+<button type="button" class="btn btn-primary">Primary</button>
+
+</body>
+</html>
